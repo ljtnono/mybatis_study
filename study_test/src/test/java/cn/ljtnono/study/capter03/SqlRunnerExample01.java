12cc886334e881a3db7e4148c56fa971f23b40e5
lingjiatong <935188400@qq.com>
2021-03-24 01:14:34 +0800

    [feat]测试sqlRunner

--- study_test/src/test/java/cn/ljtnono/study/capter03/SqlRunnerExample01.java
+++ study_test/src/test/java/cn/ljtnono/study/capter03/SqlRunnerExample01.java
@@ -1,5 +1,20 @@
 package cn.ljtnono.study.capter03;
 
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.jdbc.SQL;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.apache.ibatis.jdbc.SqlRunner;
+import org.apache.ibatis.session.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
 /**
  * 使用SqlRunner工具类执行sql语句
  *
@@ -7,4 +22,39 @@ package cn.ljtnono.study.capter03;
  * Date: 2021/3/3 12:45 上午
  */
 public class SqlRunnerExample01 {
+
+
+    private static DataSource dataSource;
+
+    @Before
+    public void pre() throws SQLException, IOException {
+        dataSource = new UnpooledDataSource("org.h2.Driver", "jdbc:h2:mem:default;MODE=MSSQLServer;", "", "");
+        Connection connection = dataSource.getConnection();
+        ScriptRunner scriptRunner = new ScriptRunner(connection);
+        // 一次性插入
+        scriptRunner.setSendFullScript(true);
+        scriptRunner.runScript(Resources.getResourceAsReader("capter03/create_table.sql"));
+        scriptRunner.runScript(Resources.getResourceAsReader("capter03/insert_data.sql"));
+    }
+
+    @Test
+    public void testSelectOne() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        SqlRunner sqlRunner = new SqlRunner(connection);
+        String sql = new SQL() {
+            {
+                SELECT("*");
+                FROM("user");
+                WHERE("id = ?");
+            }
+        }.toString();
+        Map<String, Object> resultMap = sqlRunner.selectOne(sql, "1");
+        System.out.println(resultMap);
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+
 }
--- study_test/src/test/resources/capter03/insert_data.sql
+++ study_test/src/test/resources/capter03/insert_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO user(id, name) VALUES (1, 'lingjiatong');
-INSERT INTO user(id, name) VALUES (1, 'lingjiatong1');
-INSERT INTO user(id, name) VALUES (2, 'lingjiatong2');
-INSERT INTO user(id, name) VALUES (3, 'lingjiatong3');
-INSERT INTO user(id, name) VALUES (4, 'lingjiatong4');
-INSERT INTO user(id, name) VALUES (5, 'lingjiatong5');
+INSERT INTO user(id, name) VALUES (2, 'lingjiatong1');
+INSERT INTO user(id, name) VALUES (3, 'lingjiatong2');
+INSERT INTO user(id, name) VALUES (4, 'lingjiatong3');
+INSERT INTO user(id, name) VALUES (5, 'lingjiatong4');
+INSERT INTO user(id, name) VALUES (6, 'lingjiatong5');
